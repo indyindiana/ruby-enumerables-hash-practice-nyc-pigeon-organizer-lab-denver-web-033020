54cd47ad716cdcb8eb9e8a2a30f57668507f8159
indiana danaher <indiana.danaher@gmail.com>
2020-03-21 01:40:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,17 @@
 def nyc_pigeon_organizer(data)
   pigeons = {}
-  
+  data.each do |color_gender_lives, value|
+    value.each do |stats, all_names|
+      all_names.each do |name|
+        if pigeons[name] == nil 
+          pigeons[name] = {}
+        end
+        if pigeons[name][color_gender_lives] == nil 
+          pigeons[name][color_gender_lives] = []
+        end
+        pigeons[name][color_gender_lives].push(stats.to_s)
+      end 
+    end 
+  end
   # write your code here!
 end
